d927be1f223748fc4cd8a809cecbfb34d1860b74
Zihan-H <hoquezihan@gmail.com>
2026-08-13 13:29:56 -0400

    g

--- wordle-stat-tracker.py
+++ wordle-stat-tracker.py
@@ -50,7 +50,7 @@ class WGuild():
         global raw
         data = json.loads(raw)
         currdate = dt.date(2022, 8, 12)
-        for day in range(419, self.calendar[list(self.calendar.keys())[0]].wordlenum):
+        for day in range(419, self.calendar[list(self.calendar.keys())[0]].wordlenum + 1):
             if currdate not in self.sglobal:
                 if str(day) in data:
                     wordle = data[str(day)]
